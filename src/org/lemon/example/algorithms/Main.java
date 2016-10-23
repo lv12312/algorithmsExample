2376d2e13ce300294867e52b85b669a847e867e3
lv12312 <lv-12312@163.com>
2016-10-23 20:11:14 +0800

    init project.

---
+++ src/org/lemon/example/algorithms/Main.java
@@ -0,0 +1,10 @@
+package org.lemon.example.algorithms;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+        System.out.println(Double.POSITIVE_INFINITY);
+        System.out.println(Double.NEGATIVE_INFINITY);
+    }
+}
